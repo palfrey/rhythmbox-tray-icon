9c5e7efec8cc5b2ce4f8e3c6634b0922bbae1a59
Tom Parker-Shemilt <palfrey@tevp.net>
2019-11-18 22:46:16 +0000

    Dockerised testing

---
+++ test.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+line_pattern = re.compile("\(rhythmbox:\d+\): (?P<group>.+)-(?P<priority>.+) \*\*: \d+:\d+:\d+\.\d+: (?P<message>.+)")
+
+problems = False
+for line in open(sys.argv[1]).readlines():
+    if line.strip() == "":
+        continue
+    items = line_pattern.search(line).groupdict()
+    if items['group'] in ['dbind', 'libdmapsharing']:
+        continue
+    if items['message'].startswith("Unable to start mDNS browsing"):
+        continue
+    if items['message'].startswith("Unable to grab media player keys"):
+        continue
+    problems = True
+    print(items)
+
+if problems:
+    sys.exit(-1)
